1e53fda6b0d2ac5db322d3810c225df8fd307810
Harshit Pratap Singh <86252946+Harshit1047@users.noreply.github.com>
2022-05-05 01:14:08 +0530

    Add files via upload

---
+++ Fuzzy Logic/Fuzzylogic.py
@@ -0,0 +1,29 @@
+pip install fuzzylogic
+from fuzzylogic.classes import Domain, Set
+from fuzzylogic.functions import bounded_linear
+from fuzzylogic.rules import rescale, round_partial
+
+rating = Domain("ratings", 1, 10, res=0.1)
+rating.norm = Set(bounded_linear(1,10))
+
+weights = {"beverage": 0.3, 
+           "atmosphere": 0.2, 
+           "looks":0.2,
+           "taste": 0.3}
+
+def weighted_sum(weights, target):
+    rsc = rescale(target._low, target._high)
+    
+    def f(factors):
+        result = sum(r * weights[n] for n, r in factors.items())
+        return round_partial(rsc(result), target._res)
+    return f
+
+
+r = weighted_sum(weights=weights, target=rating)
+
+ratings = {"beverage": rating.min(5),
+           "atmosphere": rating.min(6),
+           "looks": rating.min(4),
+            "taste": rating.min(10)}
+r(ratings)
